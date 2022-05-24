bad2204f923d294d98610376a01ed5ee56823649
Cesar Castillo <ccastillo@datafierce.com>
2022-05-23 20:06:05 -0500

    viewChildren

---
+++ src/app/pages/view-child/card/card.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input() header: string='Header ';
+  @Input() title: string= 'Title';
+  @Input() description: string= 'Description';
+  public buttons: any[]=[];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  public addButton( type: string, title: string){
+    this.buttons.push({type, title});
+  }
+
+  public setHeader(header: string){
+    this.header= header;
+  }
+
+}
--- src/app/pages/view-child/view-child.component.ts
+++ src/app/pages/view-child/view-child.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
 import { TitleComponent } from './title/title.component';
+
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-view-child',
@@ -10,7 +12,34 @@ export class ViewChildComponent implements OnInit, AfterViewInit {
 
   @ViewChild('title') titleElement: ElementRef | undefined;
 
-  @ViewChild(TitleComponent, {static: true}) titleCompenent: TitleComponent  | undefined;
+  @ViewChild(TitleComponent, { static: true }) titleCompenent: TitleComponent | undefined;
+
+
+  lista: string[] = ['Benito Juarez', 'Miguel Hidalgo', 'Franciso I Madero', 'Jose Maria Morles'];
+
+  @ViewChildren('listaElement') listaElement: QueryList<ElementRef> | undefined;
+  @ViewChildren(CardComponent) listaComponents: QueryList<CardComponent> | undefined;
+
+
+
+  listaCards: { header: string; title: string; description: string;}[] = [
+    {
+      header: 'Header 1',
+      title: 'Title 1',
+      description: 'Descritpion 1'
+    }, {
+      header: 'Header 2',
+      title: 'Title 2',
+      description: 'Descritpion 2'
+    }, {
+      header: 'Header 3',
+      title: 'Title 3',
+      description: 'Descritpion 3'
+    }, {
+      header: 'Header 4',
+      title: 'Title 4',
+      description: 'Descritpion 4'
+    }]
 
 
 
@@ -29,17 +58,34 @@ export class ViewChildComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
 
-    if(this.titleElement){
+    if (this.titleElement) {
       this.titleElement.nativeElement.style.color = 'green';
       this.titleElement.nativeElement.style.fontSize = '14px';
 
     }
 
+  }
+
+  cambiarColor() {
+
+    this.listaElement?.forEach((item: ElementRef) => {
+      item.nativeElement.style.color = 'green';
+    })
 
 
+  }
+
+  ponerBotones(){
+
+    this.listaComponents?.get(0)?.addButton('success', 'Enviar');
+    this.listaComponents?.get(1)?.addButton('danger', 'Borrar');
+
+    this.listaComponents?.get(1)?.setHeader('Header 2');
 
 
 
   }
 
+
 }
+
